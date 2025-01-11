9e7b0d1b7347c2f9e2a312301a20259812ec312a
lhy0118 <lhy0118@midasit.com>
2025-01-11 14:44:20 +0900

    except test folder when project is build and gitignore, eslint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
+  webpack: (config) => {
+    // test 폴더 무시
+    config.watchOptions = {
+      ignored: ["**/test/**"],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
